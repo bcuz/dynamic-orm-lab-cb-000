bdbb599fb94254dd2cee8f588e97122dccf7445d
adam n <adam.nawroc@gmail.com>
2018-03-19 13:10:01 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
   def self.find_by(name)
     binding.pry
     # getting specif keyword from paramter
-    # sql = "SELECT * FROM #{self.table_name} WHERE #{stuff.keys[0].to_s} = '#{stuff}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{stuff.keys[0].to_s} = '#{stuff}'"
     # sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     # DB[:conn].execute(sql)
   end
